afaa88fc2c88f508a7e5ff8033fb5febb38a84cd
Sebastián Salvatierra <focusyn.2506@gmail.com>
2023-01-04 00:28:59 -0300

    mejora de lógica con funciones para un código más limpio y claro

--- script.js
+++ script.js
@@ -2,59 +2,51 @@ function aleatorio (min, max){
     return Math.floor(Math.random() * (max - min +1 ) + min)
 }
 
-let user = prompt('Elije:\n1 - piedra \n2 - tijera\n3 - papel');
+function eleccion (jugada){
+    let resultado = '';
+    if (jugada == 1){
+        resultado = 'Elegiste 🥌'
+    } else if (jugada == 2){
+        resultado = 'Elegiste 🧻'
+    } else if (jugada == 3){
+        resultado = 'Elegiste ✂'
+    } else{
+        resultado = 'Elegiste PERDER😧'
+    }
+    return resultado;
+};
 
-cpu = aleatorio(1,3);
+function eleccionCPU (jugada){
+    let resultado = '';
+    if (jugada == 1){
+        resultado = 'Elegiste 🥌'
+    } else if (jugada == 2){
+        resultado = 'Elegiste 🧻'
+    } else if (jugada == 3){
+        resultado = 'Elegiste ✂'
+    } 
+    return resultado;
+};
 
+function combate (user, cpu){
+    let resultado = '';
+    if (user == 2 && cpu == 1||user == 1 && cpu == 3 ||user == 3 && cpu == 2){
+        resultado = 'ganaste'
+    }  else {
+        resultado = 'perdiste'
+    }
+    return resultado;
+};
 
-if (user == 1){
-    opcion = 'Elegiste piedra'
-    alert(opcion);
-}
-if (user == 2){
-    opcion = 'Elegiste papel'
-    alert(opcion);
-}
-if (user == 3){
-    opcion = 'Elegiste tijera'
-    alert(opcion);
-} 
+let user = prompt('Elije:\n1 - piedra \n2 - papel\n3 - tijera');
+let cpu = aleatorio(1,3);
 
-if (cpu == 1){
-    opcion = 'CPU elige piedra'
-    alert(opcion);
-}
-if (cpu == 2){
-    opcion = 'CPU elige papel'
-    alert(opcion);
-}
-if (cpu == 3){
-    opcion = 'CPU elige tijera'
-    alert(opcion);
-} 
+//ELECCIÓN 
+alert('Elegiste: ' + eleccion(user));
+alert('El CPU eligió: ' + eleccionCPU (cpu));
 
-
-if (user == 1 && cpu == 2){
-    alert('perdiste')
-}
-if (user == 2 && cpu == 3){
-    alert('ganaste')
-}
-if (user == 2 && cpu == 1){
-    alert('ganaste')
-}
-if (user == 3 && cpu == 1){
-    alert('perdiste')
-}
-if (user == 3 && cpu == 2){
-    alert('perdiste')
-}
-if (user == 1 && cpu == 3){
-    alert('ganaste')
-}
-if (user == cpu){
-    alert ('empate')
-}
+//COMBATE
+alert(combate(user, cpu));
 
 console.log('player eligió: ' + user)
 console.log('cpu eligió: ' + cpu);
